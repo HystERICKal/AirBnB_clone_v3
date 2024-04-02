d5f391a125e25ca28382e8e290126f28589c1b52
HystERICKal <nyoroerick12@gmail.com>
2024-04-02 06:49:40 -0700

    Task 6 err_v1

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """Create State Objects."""
 
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request, make_response
 from models.state import State
 from models import storage
 
