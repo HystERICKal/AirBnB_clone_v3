8d554e430c72aa48c9387d0e18471c90084178e2
HystERICKal <nyoroerick12@gmail.com>
2024-04-02 06:35:59 -0700

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Create new flask app here."""
+"""Make the api start."""
 
-from os import getenv
-from flask import Flask
-# from models import storage
+from flask import Flask, jsonify, make_response
+from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove and close session."""
+    storage.close()
+
+
+@app.errorhandler(404)
+def errorhandler(error):
+    """Handle the error 404."""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', '5000')))
 
