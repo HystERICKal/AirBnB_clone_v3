e7c974143324fe0079113ef0290c724346238837
HystERICKal <nyoroerick12@gmail.com>
2024-04-02 06:12:02 -0700

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
-"""Make a new flask app here."""
+"""Handle starting of api."""
 
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status')
-def api_status():
-    """Return the api status."""
-    response = {'status': "OK"}
-    return jsonify(response)
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """Return JSON status."""
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def stats():
+    """Retrieve number of each obj by type."""
+    classes = {
+        'amenities': 'Amenity',
+        'cities': 'City',
+        'places': 'Place',
+        'reviews': 'Review',
+        'states': 'State',
+        'users': 'User'
+    }
+    reslt = {}
+    for i, j in classes.items():
+        reslt[i] = storage.count(j)
+    return jsonify(reslt)
+
+
+if __name__ == '__main__':
+    pass
 
